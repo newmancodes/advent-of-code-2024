d379e6704f202047a9598f41dba33ca0d327b7fb
Stephen Newman <steve@newman.digital>
2024-11-30 20:27:34 +0000

    Run tests (nicely?)

--- test_day01.py
+++ test_day01.py
@@ -1,5 +1,10 @@
 import day01
+import pytest
+import sys
 
 def test_greeting_is_hello():
     puzzle = day01.Day01Puzzle()
     assert puzzle.greeting() == "hello"
+
+if __name__ == "__main__":
+    sys.exit(pytest.main(["./test_day01.py"]))
