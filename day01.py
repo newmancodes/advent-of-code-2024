88804cd43c08f84f7ddec3b1f8625703cb3ecac4
Stephen Newman <steve@newman.digital>
2024-12-01 08:45:31 +0000

    Day 01 - WIP - First part, example.

--- day01.py
+++ day01.py
@@ -1,7 +1,15 @@
-class Day01Puzzle():
-    def greeting(self) -> str:
-        return "hello"
+class ListComparer():
+    def calculate_distance(self, x: list[int], y: list[int]) -> int:
+        x.sort()
+        y.sort()
+
+        total_distance: int = 0
+
+        for index, x_item in enumerate(x): # Assumes x and y have the same length
+            position_distance = abs(x_item - y[index])
+            total_distance = total_distance + position_distance
+
+        return total_distance
 
 if __name__ == "__main__":
-    puzzle = Day01Puzzle()
-    print(puzzle.greeting())
+    print("implement me")
--- test_day01.py
+++ test_day01.py
@@ -2,9 +2,12 @@ import day01
 import pytest
 import sys
 
-def test_greeting_is_hello():
-    puzzle = day01.Day01Puzzle()
-    assert puzzle.greeting() == "hello"
+def test_initial_example_has_a_distance_of_11():
+    left_list = list((3, 4, 2, 1, 3, 3))
+    right_list = list((4, 3, 5, 3, 9, 3))
+
+    listComparer = day01.ListComparer()
+    assert listComparer.calculate_distance(left_list, right_list) == 11
 
 if __name__ == "__main__":
     sys.exit(pytest.main(["./test_day01.py"]))
