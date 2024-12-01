3caea21ee0f90c5b933681a82dad5749a411271b
Stephen Newman <steve@newman.digital>
2024-12-01 09:07:37 +0000

    Day 01 - WIP - First part, answered.

--- day01.py
+++ day01.py
@@ -1,15 +1,33 @@
-class ListComparer():
-    def calculate_distance(self, x: list[int], y: list[int]) -> int:
+class ListComparer:
+    def calculate_distance(x: list[int], y: list[int]) -> int:
         x.sort()
         y.sort()
 
-        total_distance: int = 0
+        total_distance = 0
 
-        for index, x_item in enumerate(x): # Assumes x and y have the same length
+        for index, x_item in enumerate(x):  # Assumes x and y have the same length
             position_distance = abs(x_item - y[index])
             total_distance = total_distance + position_distance
 
         return total_distance
 
+
+class InputParser:
+    def parse_file(filename: str) -> tuple[list[int], list[int]]:
+        left_list = []
+        right_list = []
+
+        with open(filename) as file:
+            for line in file:
+                numbers = line.split("   ")
+                left_list.append(int(numbers[0]))
+                right_list.append(int(numbers[1]))
+
+        return (left_list, right_list)
+
+
 if __name__ == "__main__":
-    print("implement me")
+    first_input_filename = "./inputs/day01/first_input.txt"
+    left_list, right_list = InputParser.parse_file(first_input_filename)
+    distance = ListComparer.calculate_distance(left_list, right_list)
+    print(f"Distance between the lists in {first_input_filename} is {distance}")
--- test_day01.py
+++ test_day01.py
@@ -2,12 +2,22 @@ import day01
 import pytest
 import sys
 
-def test_initial_example_has_a_distance_of_11():
-    left_list = list((3, 4, 2, 1, 3, 3))
-    right_list = list((4, 3, 5, 3, 9, 3))
 
-    listComparer = day01.ListComparer()
-    assert listComparer.calculate_distance(left_list, right_list) == 11
+def test_example_input_can_be_parsed():
+    left_list, right_list = day01.InputParser.parse_file(
+        "./inputs/day01/example_input.txt"
+    )
+
+    assert left_list == [3, 4, 2, 1, 3, 3]
+    assert right_list == [4, 3, 5, 3, 9, 3]
+
+
+def test_initial_example_has_a_distance_of_11():
+    left_list = [3, 4, 2, 1, 3, 3]
+    right_list = [4, 3, 5, 3, 9, 3]
+
+    assert day01.ListComparer.calculate_distance(left_list, right_list) == 11
+
 
 if __name__ == "__main__":
     sys.exit(pytest.main(["./test_day01.py"]))
