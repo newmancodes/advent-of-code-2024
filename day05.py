475381b4f3b3435ce71a5d441b674b442381e0ff
Stephen Newman <steve@newman.digital>
2024-12-05 10:04:52 +0000

    Day 05 - WIP - Part 1 Complete.

--- day05.py
+++ day05.py
@@ -1,2 +1,116 @@
+import math
+from typing import Tuple
+
+
+class OrderingRule:
+    def __init__(self, first: int, next: int):
+        self.__first = first
+        self.__next = next
+
+    def get_first(self) -> int:
+        return self.__first
+
+    def get_next(self) -> int:
+        return self.__next
+
+    def is_applicable(self, pages: list[int]) -> bool:
+        return self.get_first() in pages and self.get_next() in pages
+
+    def is_satisfied(self, pages: list[int]) -> bool:
+        first = self.get_first()
+        next = self.get_next()
+
+        location_of_first = [
+            index for index, value in enumerate(pages) if value == first
+        ]
+        location_of_next = [index for index, value in enumerate(pages) if value == next]
+
+        return location_of_first < location_of_next
+
+
+class Update:
+    def __init__(self, updated_pages: list[int]):
+        self.__updated_pages = updated_pages
+
+    def __check_applicable(self, ordering_rule: OrderingRule) -> bool:
+        return ordering_rule.is_applicable(self.get_updated_pages())
+
+    def __check_satisfied(self, ordering_rule: OrderingRule) -> bool:
+        return ordering_rule.is_satisfied(self.get_updated_pages())
+
+    def get_updated_pages(self) -> list[int]:
+        return self.__updated_pages
+
+    def should_be_printed(self, ordering_rules: list[OrderingRule]) -> bool:
+        applicable_rules = list(filter(self.__check_applicable, ordering_rules))
+        satisfied_rules = list(filter(self.__check_satisfied, applicable_rules))
+
+        for applicable_rule in applicable_rules:
+            if not applicable_rule in satisfied_rules:
+                return False
+
+        return True
+
+    def get_middle_page_number(self) -> int:
+        updated_pages = self.get_updated_pages()
+        return self.get_updated_pages()[math.trunc(len(updated_pages) / 2)]
+
+
+class Instruction:
+    def __init__(self, ordering_rules: list[OrderingRule], updated: list[Update]):
+        self.__ordering_rules = ordering_rules
+        self.__updated = updated
+
+    def get_ordering_rules(self) -> list[OrderingRule]:
+        return self.__ordering_rules
+
+    def get_updated(self) -> list[Update]:
+        return self.__updated
+
+    def __should_be_printed(self, update: Update) -> bool:
+        return update.should_be_printed(self.get_ordering_rules())
+
+    def sum_middle_page_of_printed_update(self) -> int:
+        return sum(
+            update.get_middle_page_number()
+            for update in list(filter(self.__should_be_printed, self.get_updated()))
+        )
+
+
+class InputParser:
+    def parse_file(filename: str) -> Instruction:
+        lines = []
+
+        with open(filename) as file:
+            for line in file:
+                lines.append(line)
+
+        return InputParser.parse_input(lines)
+
+    def parse_input(input: list[str]) -> Instruction:
+        am_parsing_ordering_rules = True
+        am_parsing_updates = False
+
+        ordering_rules = []
+        updated = []
+
+        for line in input:
+            if line == "\n":
+                am_parsing_ordering_rules = False
+                am_parsing_updates = True
+                continue
+            elif am_parsing_ordering_rules == True:
+                rule_components = line.split("|")
+                ordering_rules.append(
+                    OrderingRule(int(rule_components[0]), int(rule_components[1]))
+                )
+            elif am_parsing_updates == True:
+                updated.append(Update(list(map(int, line.split(",")))))
+
+        return Instruction(ordering_rules, updated)
+
+
 if __name__ == "__main__":
-    print("Hello, World!")
+    instruction = InputParser.parse_file("./inputs/day05/first_input.txt")
+    sum_of_middle_pages = instruction.sum_middle_page_of_printed_update()
+    print(f"Calculated that the middle pages sum to {sum_of_middle_pages}")
--- test_day05.py
+++ test_day05.py
@@ -2,5 +2,126 @@ import day05
 import pytest
 import sys
 
+
+def test_middle_page_can_be_located():
+    update = day05.Update([75, 47, 61, 53, 29])
+
+    assert update.get_middle_page_number() == 61
+
+
+def test_applicable_ordering_rule_is_detected():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(47, 53)
+
+    assert sut.is_applicable(pages) == True
+
+
+def test_inapplicable_ordering_rule_is_detected_zero_matches():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(32, 90)
+
+    assert sut.is_applicable(pages) == False
+
+
+def test_inapplicable_ordering_rule_is_detected_only_matches_first():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(47, 90)
+
+    assert sut.is_applicable(pages) == False
+
+
+def test_inapplicable_ordering_rule_is_detected_only_matches_next():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(32, 53)
+
+    assert sut.is_applicable(pages) == False
+
+
+def test_satisfied_ordering_rule_is_detected():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(47, 53)
+
+    assert sut.is_satisfied(pages) == True
+
+
+def test_unsatisfied_ordering_rule_is_detected():
+    pages = [75, 47, 61, 53, 29]
+
+    sut = day05.OrderingRule(53, 47)
+
+    assert sut.is_satisfied(pages) == False
+
+
+def test_printable_update_is_detected():
+    ordering_rules = [
+        day05.OrderingRule(47, 53),
+        day05.OrderingRule(97, 13),
+        day05.OrderingRule(97, 61),
+        day05.OrderingRule(97, 47),
+        day05.OrderingRule(75, 29),
+        day05.OrderingRule(61, 13),
+        day05.OrderingRule(75, 53),
+        day05.OrderingRule(29, 13),
+        day05.OrderingRule(97, 29),
+        day05.OrderingRule(53, 29),
+        day05.OrderingRule(61, 53),
+        day05.OrderingRule(97, 53),
+        day05.OrderingRule(61, 29),
+        day05.OrderingRule(47, 13),
+        day05.OrderingRule(75, 47),
+        day05.OrderingRule(97, 75),
+        day05.OrderingRule(47, 61),
+        day05.OrderingRule(75, 61),
+        day05.OrderingRule(47, 29),
+        day05.OrderingRule(75, 13),
+        day05.OrderingRule(53, 13),
+    ]
+    sut = day05.Update([75, 47, 61, 53, 29])
+
+    assert sut.should_be_printed(ordering_rules) == True
+
+
+def test_initial_example_has_a_total_of_143():
+    input = [
+        "47|53",
+        "97|13",
+        "97|61",
+        "97|47",
+        "75|29",
+        "61|13",
+        "75|53",
+        "29|13",
+        "97|29",
+        "53|29",
+        "61|53",
+        "97|53",
+        "61|29",
+        "47|13",
+        "75|47",
+        "97|75",
+        "47|61",
+        "75|61",
+        "47|29",
+        "75|13",
+        "53|13",
+        "",
+        "75,47,61,53,29",
+        "97,61,53,29,13",
+        "75,29,13",
+        "75,97,47,61,53",
+        "61,13,29",
+        "97,13,75,29,47",
+    ]
+
+    sut = day05.InputParser.parse_input(input)
+
+    assert sut.sum_middle_page_of_printed_update() == 143
+
+
 if __name__ == "__main__":
     sys.exit(pytest.main(["./test_day05.py"]))
